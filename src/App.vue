eeba78030a8b5f5062d4db0d481d145b2bd4eb8f
Lucas Fletcher <lfletcher27@ivytech.edu>
2022-06-24 15:22:55 -0400

    final commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <template>
-  
+  <div id="main-app" class="container">
+    <h4>{{ title }}</h4>
+    <font-awesome-icon icon="plus" />Add Appointment
+    <div v-for="(item, i) in appointments" v-bind:key="i">
+      <h4>{{item.petName}} </h4>
+      <p>{{item.aptNotes}} </p>
+    </div>
+  </div>
 </template>
 
 <script>
-
+import axios from "axios";
 
 export default {
-  name: 'App',
-}
+  name: 'MainApp',
+  data: function () {
+    return {
+      title: "Appointment List",
+      appointments: []
+    };
+  },
+  mounted() {
+    axios
+      .get("./data/appointments.json")
+      .then(response => (this.appointments = response.data))
+  }
+};
 </script>
-
